e4dc14ad9c0fbc9ace0d503236d0a5a2cb9fbf78
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-09-30 09:10:10 +0700

    feat: renew home page

--- Week 5/superhero_jayaku/lib/pages/home.dart
+++ Week 5/superhero_jayaku/lib/pages/home.dart
@@ -1,51 +1,99 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:superhero_jayaku/components/app_raised_button.dart';
+import 'package:superhero_jayaku/components/app_icon_button.dart';
 
-class Home extends StatelessWidget {
-  static const String routeId = 'home';
+class Home extends StatefulWidget {
+  static const String _routeId = 'home';
+
+  static String get id => _routeId;
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
 
   final _auth = FirebaseAuth.instance;
+  User loggedIn;
+  String _profileName = "John Doe";
+  String _profileImageUrl = "https://www.pngkey.com/png/detail/230-2301779_best-classified-apps-default-user-profile.png";
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserProfile();
+  }
+
+  void _getUserProfile() {
+    User user = _auth.currentUser;
+
+    if (user != null) {
+      loggedIn = user;
+      if (loggedIn.displayName != null) {
+        _profileName = loggedIn.displayName;
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 24.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              SvgPicture.asset(
-                "assets/hero.svg",
-                width: MediaQuery.of(context).size.width * 0.75,
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                    _profileImageUrl,
+                    scale: 0.2,
+                ),
+                radius: 80.0,
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 16.0),
-                child: Text(
-                  'Logged In!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'Quicksand',
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.w900,
+              SizedBox(height: 20.0),
+              Text(
+                _profileName.toString(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Quicksand',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      AppIconButton(
+                        tooltip: 'Set Profile Name',
+                        icon: Icon(Icons.settings_applications),
+                        onPressed: () {},
+                        color: Colors.blue,
+                      ),
+                      IconButton(
+                        tooltip: 'Create Moods',
+                        icon: Icon(Icons.person_add),
+                        onPressed: () {},
+                        color: Colors.amber,
+                      ),
+                      IconButton(
+                        tooltip: 'Sign Out',
+                        icon: Icon(Icons.block),
+                        onPressed: () async {
+                          await _auth.signOut();
+                          Navigator.pushReplacementNamed(context, 'sign_in');
+                        },
+                        color: Colors.red[700],
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 80.0),
-                child: AppRaisedButton(
-                  color: Color.fromRGBO(0, 101, 255, 1),
-                  press: () async {
-                    await _auth.signOut();
-                    Navigator.pushNamed(context, 'sign_in');
-                  },
-                  text: 'Sign Out',
-                  textColor: Color.fromRGBO(255, 255, 255, 1),
-                ),
-              ),
+              )
             ],
           )
         )
