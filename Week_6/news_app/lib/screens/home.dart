feaaed7b7b2eee6f976ec5c5f4c250c320bd85d4
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-11-11 00:50:22 +0700

    feat: add sign out button

--- Week_6/news_app/lib/screens/home.dart
+++ Week_6/news_app/lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/screens/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String _id = 'home';
@@ -9,10 +11,55 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String _username = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _getStore();
+  }
+
+  void _getStore() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _username = prefs.getString('username');
+    });
+  }
+
+  void _signOut() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+    Navigator.pushNamed(context, LoginScreen.id);
+  }
+
+  static const TextStyle _textStyle = TextStyle(fontFamily: 'Roboto', fontSize: 36.0);
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Welcome " + _username,
+                textAlign: TextAlign.center,
+                style: _textStyle,
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 24.0, 0, 0),
+                child: RaisedButton(
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  textColor: Colors.white,
+                  color: Colors.black87,
+                  child: Text("SIGN OUT"),
+                  onPressed: () => _signOut(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
