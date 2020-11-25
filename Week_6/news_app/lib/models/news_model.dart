3942f99f08f09180500a575484f20459c953b81d
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-11-25 20:01:47 +0700

    feat: create simple NewsModel

---
+++ Week_6/news_app/lib/models/news_model.dart
@@ -0,0 +1,21 @@
+class NewsModel {
+  NewsModel({
+    this.newsId,
+    this.image,
+    this.title,
+    this.content,
+    this.description,
+    this.dateNews,
+    this.userId,
+    this.username,
+  });
+
+  String newsId;
+  String image;
+  String title;
+  String content;
+  String description;
+  String dateNews;
+  String userId;
+  String username;
+}
